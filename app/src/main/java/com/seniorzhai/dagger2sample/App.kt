294f0fc718fb2f20138632822c97bad1bce93aa6
zhaitao <370985116@qq.com>
2017-10-16 16:02:12 +0800

    LeakCanary & Logger

--- app/src/main/java/com/seniorzhai/dagger2sample/App.kt
+++ app/src/main/java/com/seniorzhai/dagger2sample/App.kt
@@ -2,9 +2,12 @@ package com.seniorzhai.dagger2sample
 
 import android.app.Application
 import android.content.Context
+import com.luseen.logger.LogType
+import com.luseen.logger.Logger
 import com.seniorzhai.dagger2sample.reject.components.AppComponent
 import com.seniorzhai.dagger2sample.reject.components.DaggerAppComponent
 import com.seniorzhai.dagger2sample.reject.module.ApiServiceModule
+import com.squareup.leakcanary.LeakCanary
 
 /**
  * Created by zhai on 16/5/23.
@@ -19,6 +22,15 @@ class App : Application() {
         // 注入AppModule ApiServiceModule
         appComponent = DaggerAppComponent.builder()
                 .apiServiceModule(ApiServiceModule())
+                .build()
+        if (LeakCanary.isInAnalyzerProcess(this))
+            return
+        LeakCanary.install(this)
+        Logger.Builder()
+                .isLoggable(BuildConfig.DEBUG)
+                .logType(LogType.ERROR)
+                .tag("Logger")
+                .setIsKotlin(true)
                 .build()
     }
 
